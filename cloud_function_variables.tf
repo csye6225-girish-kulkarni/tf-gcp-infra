b5b32c46fdca927a1c8bf3f6871cfbfa11532e9a
girish332 <kulkarni.gi@northeastern.edu>
2024-03-27 12:42:53 -0400

    Added Cloud Function , PubSub Setup

---
+++ cloud_function_variables.tf
@@ -0,0 +1,9 @@
+variable "mailgun_api_key" {
+  description = "Api key for mailgun"
+  default     = "f97930cde56cb1af3433144b9c3f769f-309b0ef4-f22d61b6"
+}
+
+variable "mailgun_domain" {
+  description = "Domain for mailgun"
+  default     = "girishkulkarni.me"
+}
